997481170c44c2d3e6b760adf1b03d4355c2405f
Jacky Ho <jackyho97@hotmail.com>
2018-01-10 14:50:23 -0500

    returns made object

--- restapi/dao.py
+++ restapi/dao.py
@@ -63,7 +63,10 @@ class BespokeDao:
             db = self.db.fruits
             modified = db.update_one({'_id': ObjectId(_id)},
                                      {'$set': {'name':name, 'sweetness': sweetness}})
-            find = db.find_one({'_id': ObjectId(_id)})
+            if modified.acknowledged:
+                return {'name': name, 'sweetness': sweetness, '_id': _id}
+            else:
+                return None
             return find if find is not None else None
         except Exception as e:
             print(e, file=sys.stderr)
