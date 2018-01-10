5d8980c8156bb01f7879faa316812c9b28bb3020
Jeremy <jmills-bespoke@users.noreply.github.com>
2018-01-10 10:00:45 -0500

    app fixes requested

--- restapi/app.py
+++ restapi/app.py
@@ -9,13 +9,20 @@ parser = reqparse.RequestParser()
 parser.add_argument('sweetness', type=int, help='Sweetness must be int')
 parser.add_argument('name')
 
+# TODO: all of the model calls should be wrapped within a try/except block.
+# TODO: if an exception happens within the request we want to be able to log it, handle it, and return the error gracefully
+
 class BespokeApp(Resource):
+    # TODO: change this to ID, and validate the ID is proper
     def get(self, name):
         model = BespokeModel()
         result = model.get_by_name(name)
         return result
 
     def post(self, name):
+        # TODO: post should exist in another class within this file
+        # TODO: one class deals with a single entity while the other deals with a collection of entities
+        # TODO: post does not take any parameters though, it just takes a payload
         model = BespokeModel()
         args = parser.parse_args()
         sweetness = args['sweetness']
@@ -23,6 +30,7 @@ class BespokeApp(Resource):
         return output
 
     def put(self, name):
+        # TODO: change this to ID, and validate the ID is proper
         model = BespokeModel()
         args = parser.parse_args()
         sweetness = args['sweetness']
@@ -30,6 +38,7 @@ class BespokeApp(Resource):
         return output
 
     def delete(self, name):
+        # TODO: change this to ID, and validate the ID is proper
         model = BespokeModel()
         output = model.check_and_delete(name)
         return output
