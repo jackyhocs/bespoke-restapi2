c27ba26d605640e20740c4da264746f7afc84a35
Jeremy <jmills-bespoke@users.noreply.github.com>
2018-01-10 10:26:56 -0500

    dao fixes

--- restapi/dao.py
+++ restapi/dao.py
@@ -12,7 +12,11 @@ class BespokeDao:
         fruit = db.find()
         items = []
         for i in fruit:
+            
+            # TODO: just append the entire fruit data from the DB
+            # TODO: let the model decide what information it requires
             items.append({'name': i['name'], 'sweetness': i['sweetness']})
+            
         return items
 
     def get_by_name(self, name):
@@ -20,14 +24,20 @@ class BespokeDao:
         try:
             result = db.find_one({'name': name})
             #print(result, file=sys.stderr)
+            
+             # TODO: just return the fruit data, let the model decide what information it requires (implement this for all of below)
+             # TODO: if result is None, return None (implement this for all of the below)
             if result is None:
                 output = {'name': name, 'error': "Item not found in database"}
             else:
                 output = {'name': name, 'sweetness': result['sweetness']}
+                
             return output
         except Exception as e:
             print(e, file=sys.stderr)
             output = {'error': "Error"}
+            
+            # TODO: just log the error and raise the exception (implement this for all of the below)
             return output
 
     def check_if_exists(self, name):
