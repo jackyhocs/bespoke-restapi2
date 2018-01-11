d95ac88f61f50c8598ffaeafa33ca9fc52a4d06e
Jacky Ho <jackyho97@hotmail.com>
2018-01-11 10:00:10 -0500

    Type check for request json

--- restapi/app.py
+++ restapi/app.py
@@ -23,10 +23,10 @@ class BespokeApp(Resource):
                     fruit = {'name': result.name, 'sweetness': result.sweetness, '_id': result._id}
                 return fruit
             else:
-                return make_response(jsonify({"error": 'ID specified does not exist'}),404)
+                return make_response(jsonify({"error": 'ID specified does not exist'}), 404)
 
-        except Exception as e:
-            return e
+        except:
+            return make_response(jsonify({"error": 'Unable to retrieve item'}), 500)
 
     def post(self, _id):
         try:
@@ -34,16 +34,19 @@ class BespokeApp(Resource):
                 args = parser.parse_args()
                 sweetness = args['sweetness']
                 name = args['name']
-                result = BespokeModel.update_item(_id, name, sweetness)
-                if result is None:
-                    fruit = {'_id': _id, 'error': "Entry not updated"}
+                if name is not None and sweetness is not None and isinstance(sweetness, int) and isinstance(name, str):
+                    result = BespokeModel.update_item(_id, name, sweetness)
+                    if result is None:
+                        fruit = {'_id': _id, 'error': "Entry not updated"}
+                    else:
+                        fruit = {'name': result.name, 'sweetness': result.sweetness, '_id': result._id}
+                    return fruit
                 else:
-                    fruit = {'name': result.name, 'sweetness': result.sweetness, '_id': result._id}
-                return fruit
+                    return make_response(jsonify({'error': 'Parameters provided are invalid'}), 404)
             else:
                 return make_response(jsonify({"error": 'ID specified does not exist'}), 404)
-        except Exception as e:
-            return e
+        except:
+            return make_response(jsonify({"error": 'Unable to update item'}), 500)
 
     def delete(self, _id):
         try:
@@ -56,19 +59,17 @@ class BespokeApp(Resource):
                 return fruit
             else:
                 return make_response(jsonify({"error": 'ID specified does not exist'}), 404)
-        except Exception as e:
-            return e
+        except:
+            return make_response(jsonify({"error": 'Unable to delete item'}), 500)
 
 
 class BespokeNoParamApp(Resource):
     def put(self):
         try:
-            print("in put", file=sys.stderr)
             args = parser.parse_args()
-            print(args, file=sys.stderr)
-            if args['name'] is not None and args['sweetness'] is not None and isinstance(args['sweetness'], int):
-                sweetness = args['sweetness']
-                name = args['name']
+            sweetness = args['sweetness']
+            name = args['name']
+            if name is not None and sweetness is not None and isinstance(sweetness, int) and isinstance(name, str):
                 if not BespokeModel.is_existing_name(name):
                     result = BespokeModel.insert_item(name, sweetness)
                     if result is None:
@@ -80,8 +81,8 @@ class BespokeNoParamApp(Resource):
                     return make_response(jsonify({"error": 'ID specified does not exist'}), 404)
             else:
                 return make_response(jsonify({'error': 'Parameters provided are invalid'}), 404)
-        except Exception:
-            return make_response(jsonify({'error': 'Parameters not provided'}), 404)
+        except:
+            return make_response(jsonify({"error": 'Unable to insert item'}), 500)
 
     def get(self):
         try:
@@ -90,8 +91,8 @@ class BespokeNoParamApp(Resource):
             for i in result:
                 json_array.append({'name': i.name, 'sweetness': i.sweetness, '_id': i._id})
             return json_array
-        except Exception as e:
-            return e
+        except:
+            return make_response(jsonify({"error": 'Unable to get all items'}), 500)
 
 
 api.add_resource(BespokeApp, '/fruits/<_id>')
